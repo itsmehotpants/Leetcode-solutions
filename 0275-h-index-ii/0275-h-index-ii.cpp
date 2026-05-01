07643cfbff5194c547fa23d5c2cf773f21f0efb4
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-05-01 18:56:02 +0530

    Time: 0 ms (100%), Space: 22.5 MB (45.85%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-
-    int f(vector<int>& c) {
-        sort(c.begin(), c.end());
+    int hIndex(vector<int>& c) {
         int n = c.size();
-        int maxi = 0;
-        for(int i = 0; i < n; i++) {
-            if(c[i] >= n - i) {
-                maxi = max(maxi, n - i);
+        int i = 0 , j = n-1;
+        int ans = 0;
+        while(i <= j){
+            int mid = i + (j-i)/2 ;
+            if(c[mid] < n-mid) i = mid + 1;
+            else if(c[mid] >= n-mid){
+                ans = n-mid;
+                j = mid - 1;
             }
         }
-        return maxi;
-    }
-
-    int hIndex(vector<int>& ar) {
-        return f(ar);
+        return ans;
     }
 };
