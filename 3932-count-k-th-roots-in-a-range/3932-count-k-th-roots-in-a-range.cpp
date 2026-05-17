943429ae073d7cbc831d277ea1a7795d3ea34abd
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-05-17 11:16:56 +0530

    Time: 1 ms (25.66%), Space: 8.9 MB (39.98%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int countKthRoots(int l, int r, int k) {
-        if(k==1){
-            return r-l+1;
-        }
-        int x = pow(r, (double)1 / k);
+        //  if(k==1){
+        //     return r-l+1;
+        // }
+        // int x = pow(r, (double)1 / k);
 
-        int ans = 0;
+        // int ans = 0;
 
-        for(int i = 0; i <= x+1; i++) {
+        // for(int i = 0; i <= x+1; i++) {
 
-            long long val = pow(i, k);
+        //     long long val = pow(i, k);
 
-            if(val >= l && val <= r) {
-                ans++;
-            }
-        }
+        //     if(val >= l && val <= r) {
+        //         ans++;
+        //     }
+        // }
 
-        return ans;
+        // return ans;
+        long long left=ceil(pow(l,1.0/k)-1e-9);
+        long long right=floor(pow(r,1.0/k)+1e-9);
+        
+        return (int)(right-left+1);
     }
 };
