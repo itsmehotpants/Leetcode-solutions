a224cb4cd1d2a5423228dde8f2213a872a211827
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-05 00:50:04 +0530

    Time: 49 ms (70.75%), Space: 24.4 MB (77.24%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,29 +3,66 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& h) {
-        const int dx[4] ={1,-1,0,0},dy[4]={0,0,1,-1};
-        int n =h.size(),m=h[0].size();
-        priority_queue<pii,vector<pii>,greater<pii>>pq;
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        dist[0][0]=0;
+
+        int n = h.size();
+        int m = h[0].size();
+
+        const int dx[4] = {1,-1,0,0};
+        const int dy[4] = {0,0,1,-1};
+
+        priority_queue<
+            pii,
+            vector<pii>,
+            greater<pii>
+        > pq;
+
+        vector<vector<int>> dist(
+            n,
+            vector<int>(m, INT_MAX)
+        );
+
+        dist[0][0] = 0;
+
         pq.push({0,{0,0}});
 
         while(!pq.empty()){
-            pii curr = pq.top();pq.pop();
-            int d=curr.first,r=curr.second.first,c=curr.second.second;
-            if(r==n-1&&c==m-1) return d;
-            for(int i =0;i<4;i++){
-                int nr=r+dx[i],nc=c+dy[i];
-                if(nr>=0&&nc>=0&&nc<m&&nr<n){
-                    int nd = max(d,abs(h[nr][nc]-h[r][c]));
-                    if(nd<dist[nr][nc]){
-                        dist[nr][nc]=nd;
+
+            auto curr = pq.top();
+            pq.pop();
+
+            int d = curr.first;
+            int r = curr.second.first;
+            int c = curr.second.second;
+
+            if(d > dist[r][c])
+                continue;
+
+            if(r == n-1 && c == m-1)
+                return d;
+
+            for(int i = 0; i < 4; i++){
+
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+
+                if(nr >= 0 && nc >= 0 &&
+                   nr < n && nc < m){
+
+                    int nd = max(
+                        d,
+                        abs(h[nr][nc] - h[r][c])
+                    );
+
+                    if(nd < dist[nr][nc]){
+
+                        dist[nr][nc] = nd;
+
                         pq.push({nd,{nr,nc}});
                     }
                 }
             }
         }
-        return 0;
 
+        return 0;
     }
 };
