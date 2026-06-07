3866c4980ede3050f1228ed8a1d5c2f09517ceb1
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-07 12:57:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (60%) - LeetHub

--- 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    bool consecutiveSetBits(int n) {
-        int ans=0;
-        while(n){
-            ans+=(n&3) ==3;
-            n>>=1;
-        }
-        return ans==1;
+    bool consecutiveSetBits(int n) {              
+        
+        return __builtin_popcount(n & (n>>1))==1;
     }
 };
