97e8833f00508ce9d75e02450633df3f71232e98
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-09 04:12:32 +0530

    Time: 4 ms (26.88%), Space: 15 MB (66.25%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> p;
+        for(auto& e: paths)
+            p.insert(e[1]);
+        for(auto& e: paths)
+            p.erase(e[0]);
+        return *p.begin(); 
+    }
+};
