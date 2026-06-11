bf8883d23cb95f12b6c9c5ddfb94788b6cb17039
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-11 19:27:55 +0530

    Time: 0 ms (100%), Space: 17.7 MB (49.69%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=2;i<n;i++){
             dp[i] = cost[i]+min(dp[i-1],dp[i-2]);
         }
-        cout<<dp[n-1]<<" "<< dp[n]<<" "<<dp[n-2]<<" "<<dp[2];
+        // cout<<dp[n-1]<<" "<< dp[n]<<" "<<dp[n-2]<<" "<<dp[2];
         return min(dp[n-2],dp[n-1]);
 
     }
