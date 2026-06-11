a8cf39880156122e0c4c95250c34170c81081a00
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-11 19:05:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+vector<int>dp;
+int solve(int n){
+        
+  dp[0]=1,dp[1]=1;
+       if(dp[n]!=-1) return dp[n];
+       return dp[n]=solve(n-1)+solve(n-2);
+
+}
+    int climbStairs(int n) {
+     dp.resize(n+1,-1);
+     return solve(n);
+    }
+};
