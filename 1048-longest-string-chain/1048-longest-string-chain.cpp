5475f1aa4dcfbcf39569e5eff4e3de03c465b46f
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-13 23:54:13 +0530

    Time: 63 ms (47.1%), Space: 17.5 MB (99.84%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+int n;
+int dp[1001];
+bool isPred(string& a,string& b){
+    int m =a.length(),n=b.length();
+    if(n-m !=1) return false;
+
+int i=0,j=0;
+while(i<m && j<n){
+    if(a[i]==b[j]) i++;
+    j++;
+}
+return i==m;
+} 
+
+static bool comp(string &a,string& b){
+     return a.length()<b.length();
+}
+int lis(vector<string>& words,int prev,int curr){
+    if(curr>=n) return 0;
+    int taken=0,not_taken=0;
+    if(dp[prev+1]!=-1) return dp[prev+1];
+    if(prev==-1 || isPred(words[prev],words[curr])){
+        taken =1+lis(words,curr,curr+1);
+    }
+    not_taken =lis(words,prev,curr+1);
+return dp[prev+1]= max(taken, not_taken);
+}
+    int longestStrChain(vector<string>& words) {
+        memset(dp,-1,sizeof(dp));
+      n= words.size();
+        sort(words.begin(),words.end(),comp);
+      return  lis(words,-1,0);
+    }
+};
