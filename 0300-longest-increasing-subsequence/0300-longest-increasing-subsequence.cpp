6b710d7b04f6799bd55646a10ce862116482b16f
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-15 20:44:34 +0530

    Time: 0 ms (100%), Space: 14.2 MB (75.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,46 @@
+// class Solution {
+// public:
+   
+//     int lengthOfLIS(vector<int>& nums) {
+// int n =nums.size();
+// vector<int>dp(n,1);
+// int ans=1;
+//     for(int i =0;i<n;i++){
+//         for(int j=0;j<i;j++){
+//             if(nums[i]>nums[j]){
+//                 dp[i] = max(dp[i],dp[j]+1);
+//                 ans= max(ans,dp[i]);
+//             }}}
+//             return ans;
+//         }
+    
+//     };
+//     // int solve(vector<int>& nums, int i, int prev_i) {
+//     //     if(i >= size(nums)) return 0;
+//     //     if(dp[prev_i+1] != -1) return dp[prev_i+1];
+//     //     int take = 0, dontTake = solve(nums, i + 1, prev_i);
+//     //     if(prev_i == -1 || nums[i] > nums[prev_i])
+//     //         take = 1 + solve(nums, i + 1, i);
+//     //     return dp[prev_i+1] = max(take, dontTake);
+//     // }
+// // };
+
+//patience sorting 
 class Solution {
 public:
-   
     int lengthOfLIS(vector<int>& nums) {
-int n =nums.size();
-vector<int>dp(n,1);
-int ans=1;
-    for(int i =0;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(nums[i]>nums[j]){
-                dp[i] = max(dp[i],dp[j]+1);
-                ans= max(ans,dp[i]);
-            }}}
-            return ans;
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); 
+            else
+                *it = nums[i];
         }
-    
-    };
-    // int solve(vector<int>& nums, int i, int prev_i) {
-    //     if(i >= size(nums)) return 0;
-    //     if(dp[prev_i+1] != -1) return dp[prev_i+1];
-    //     int take = 0, dontTake = solve(nums, i + 1, prev_i);
-    //     if(prev_i == -1 || nums[i] > nums[prev_i])
-    //         take = 1 + solve(nums, i + 1, i);
-    //     return dp[prev_i+1] = max(take, dontTake);
-    // }
-// };
+        
+        return (int)sorted.size();
+    }
+};
