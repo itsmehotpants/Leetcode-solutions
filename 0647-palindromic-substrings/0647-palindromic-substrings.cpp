71c29c3ee6b1d1894724c062545c53a3d945d91d
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-17 02:49:50 +0530

    Time: 39 ms (23.87%), Space: 34.4 MB (22.27%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-
-int t[1001][1001];
-bool isPal(int i,int j,string& s){
-if(i>j) return true;
-if(t[i][j]!=-1) return t[i][j];
-if(s[i]==s[j]) return t[i][j]= isPal(i+1,j-1,s);
-else return  t[i][j]=false;
-
-}
-
     int countSubstrings(string s) {
-        memset(t,-1,sizeof(t));
-        int n =s.length();
-        int cnt=0;
-        for(int i =0;i<n;i++){
-            for(int j =i;j<n;j++){
-                if(isPal(i,j,s)) cnt++;
+        int n = s.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int cnt = 0;
+
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i + L - 1 < n; i++) {
+
+                int j = i + L - 1;
+
+                if(i == j) {
+                    dp[i][j] = 1;
+                }
+                else if(i + 1 == j) {
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    dp[i][j] =
+                        (s[i] == s[j]) && dp[i + 1][j - 1];
+                }
+                cnt += dp[i][j];
             }
         }
         return cnt;
