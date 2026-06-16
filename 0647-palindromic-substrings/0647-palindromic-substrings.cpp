90d16a0f2d552e07a7ee9f20c5044787bf9904cb
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-17 02:51:28 +0530

    Time: 27 ms (33.13%), Space: 34.6 MB (14.46%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -6,24 +6,34 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, 0));
         int cnt = 0;
 
-        for(int L = 1; L <= n; L++) {
-            for(int i = 0; i + L - 1 < n; i++) {
+    //  for(int L = 1; L <= n; L++) {
+    //         for(int i = 0; i + L - 1 < n; i++) {
 
-                int j = i + L - 1;
+    //             int j = i + L - 1;
 
-                if(i == j) {
-                    dp[i][j] = 1;
-                }
-                else if(i + 1 == j) {
-                    dp[i][j] = (s[i] == s[j]);
-                }
-                else {
-                    dp[i][j] =
-                        (s[i] == s[j]) && dp[i + 1][j - 1];
-                }
-                cnt += dp[i][j];
-            }
-        }
+    //             if(i == j) {
+    //                 dp[i][j] = 1;
+    //             }
+    //             else if(i + 1 == j) {
+    //                 dp[i][j] = (s[i] == s[j]);
+    //             }
+    //             else {
+    //                 dp[i][j] =
+    //                     (s[i] == s[j]) && dp[i + 1][j - 1];
+    //             }
+    //             cnt += dp[i][j];
+    //         }
+    //     }
+    for(int L=1; L<=n; L++){
+    for(int i=0; i+L-1<n; i++){
+        int j=i+L-1;
+
+        if(s[i]==s[j] && (L<=2 || dp[i+1][j-1]))
+            dp[i][j]=1;
+
+        cnt += dp[i][j];
+    }
+}
         return cnt;
     }
 };
