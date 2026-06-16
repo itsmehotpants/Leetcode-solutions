f279805211e5d91471105a10f1f9d0a967999c27
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-17 02:13:16 +0530

    Time: 31 ms (29.39%), Space: 13 MB (37.14%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
 
+int t[1001][1001];
 bool isPal(int i,int j,string& s){
 if(i>j) return true;
-if(s[i]==s[j]) return isPal(i+1,j-1,s);
-else return false;
+if(t[i][j]!=-1) return t[i][j];
+if(s[i]==s[j]) return t[i][j]= isPal(i+1,j-1,s);
+else return  t[i][j]=false;
 
 }
 
     int countSubstrings(string s) {
+        memset(t,-1,sizeof(t));
         int n =s.length();
         int cnt=0;
         for(int i =0;i<n;i++){
