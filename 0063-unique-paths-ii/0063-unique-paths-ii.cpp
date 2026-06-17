eff804f93ef5bdac7d8a879671d12554062d174a
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-17 19:32:16 +0530

    Time: 0 ms (100%), Space: 12 MB (6.15%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    int f(int i, int j, vector<vector<int>>& mat, vector<vector<int>>& dp) {
+        int m = mat.size(), n = mat[0].size();
+
+        if (i >= m || j >= n || mat[i][j] == 1) return 0;
+
+        if (i == m - 1 && j == n - 1) return 1;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = f(i + 1, j, mat, dp) + f(i, j + 1, mat, dp);
+    }
+
     int uniquePathsWithObstacles(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) { 
-                    dp[i][j] = 0;
-                } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    int up = (i > 0) ? dp[i-1][j] : 0;
-                    int left = (j > 0) ? dp[i][j-1] : 0;
-                    dp[i][j] = up + left;
-                }
-            }
-        }
-
-        return dp[m-1][n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(0, 0, mat, dp);
     }
 };
