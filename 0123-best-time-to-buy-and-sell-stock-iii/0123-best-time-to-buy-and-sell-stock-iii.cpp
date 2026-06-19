dfe5e1d3f2a39b9a3022e080e7d6f5f8fdbe3ccd
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-19 06:51:46 +0530

    Time: 51 ms (65.56%), Space: 79.3 MB (75.88%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -16,20 +16,37 @@ public:
 
     int maxProfit(vector<int>& a) {
        int  n  =a.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // return solve(0,0,2,a);
+    //     vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+    //     // return solve(0,0,2,a);
+    //     for(int  i=n-1;i>=0;i--){
+    //         for(int buy =0;buy<=1;buy++){
+    //             for(int cap =1;cap<=2;cap++){
+    //                  if(buy==1){
+    //      dp[i][buy][cap] = max(-a[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+    // }
+    // //sell
+    // else
+    //  dp[i][buy][cap]=max(a[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1][2];
+    vector<vector<int>>curr(2,vector<int>(3,0));
+        vector<vector<int>>after(2,vector<int>(3,0));
+  
         for(int  i=n-1;i>=0;i--){
             for(int buy =0;buy<=1;buy++){
                 for(int cap =1;cap<=2;cap++){
                      if(buy==1){
-         dp[i][buy][cap] = max(-a[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+         curr[buy][cap] = max(-a[i]+after[0][cap],after[1][cap]);
     }
     //sell
     else
-     dp[i][buy][cap]=max(a[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+     curr[buy][cap]=max(a[i]+after[1][cap-1],after[0][cap]);
                 }
             }
+            after= curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
