8e115d2fe8403f964408041410adda50e2dafa6e
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-19 09:39:55 +0530

    Time: 8 ms (90.5%), Space: 9 MB (90.81%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,25 +4,17 @@ public:
 
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-
-        vector<ll> next(m + 1, 0), curr(m + 1, 0);
-
-        next[m] = 1; 
+        vector<ll> dp(m + 1, 0);
+        dp[m] = 1;
 
         for (int i = n - 1; i >= 0; i--) {
-            curr[m] = 1;
-
-            for (int j = m - 1; j >= 0; j--) {
-                curr[j] = next[j]; 
-
+            for (int j = 0; j < m; j++) {
                 if (s[i] == t[j]) {
-                    curr[j] += next[j + 1];
+                    dp[j] += dp[j + 1];
                 }
             }
-
-            next = curr;
         }
 
-        return next[0];
+        return dp[0];
     }
 };
