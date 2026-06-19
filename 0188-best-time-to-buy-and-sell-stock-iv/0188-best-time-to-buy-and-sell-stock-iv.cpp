89a3c673f20e263ed56f326a19ac2a2f9e5deb7e
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-19 06:53:48 +0530

    Time: 0 ms (100%), Space: 14.6 MB (75.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
 
-int n;
-int solve(int  i, int buy, int cap,vector<int>& a,vector<vector<vector<int>>>&dp){
-    if(i>=n||cap==0) return 0;
-    if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
+// int n;
+// int solve(int  i, int buy, int cap,vector<int>& a,vector<vector<vector<int>>>&dp){
+//     if(i>=n||cap==0) return 0;
+//     if(curr[buy][cap]!=-1) return curr[buy][cap];
 
-    if(buy){
-        return dp[i][buy][cap] = max(-a[i]+solve(i+1,0,cap,a,dp),solve(i+1,1,cap,a,dp));
-    }
-    //sell
-    return dp[i][buy][cap]=max(a[i]+solve(i+1,1,cap-1,a,dp),solve(i+1,0,cap,a,dp));
+//     if(buy){
+//         return curr[buy][cap] = max(-a[i]+solve(i+1,0,cap,a,dp),solve(i+1,1,cap,a,dp));
+//     }
+//     //sell
+//     return curr[buy][cap]=max(a[i]+solve(i+1,1,cap-1,a,dp),solve(i+1,0,cap,a,dp));
 
-}
+// }
 
     int maxProfit(int k ,vector<int>& a) {
-         n  =a.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return solve(0,1,k,a,dp);
+          int  n  =a.size();
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // return solve(0,0,2,a);
+        vector<vector<int>>curr(2,vector<int>(k+1,0));
+        vector<vector<int>>after(2,vector<int>(k+1,0));
+  
+        for(int  i=n-1;i>=0;i--){
+            for(int buy =0;buy<=1;buy++){
+                for(int cap =1;cap<=k;cap++){
+                     if(buy==1){
+         curr[buy][cap] = max(-a[i]+after[0][cap],after[1][cap]);
+    }
+    //sell
+    else
+     curr[buy][cap]=max(a[i]+after[1][cap-1],after[0][cap]);
+                }
+            }
+            after= curr;
+        }
+        return after[1][k];
     }
 };
