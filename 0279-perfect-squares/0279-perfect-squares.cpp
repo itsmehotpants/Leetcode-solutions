c817473c8cd23b7b0477d468efa0f973ca35c252
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-23 04:41:37 +0530

    Time: 35 ms (88.51%), Space: 13.2 MB (53.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,21 +1,38 @@
+// class Solution {
+// public:
+
+// int dp[10001];
+// int solve(int n){
+//     if(n==0) return 0;
+//     if(dp[n]!=-1) return dp[n];
+//     int cnt=INT_MAX;
+
+
+//     for(int i =1;i*i<=n;i++){
+//         int res =1+ solve(n-i*i);
+//         cnt= min(cnt,res);
+//     }
+//     return dp[n]=cnt;
+// }
+//     int numSquares(int n) {
+//         memset(dp,-1,sizeof(dp));
+//         return solve(n);
+//     }
+// };
+
 class Solution {
 public:
-
-int dp[10001];
-int solve(int n){
-    if(n==0) return 0;
-    if(dp[n]!=-1) return dp[n];
-    int cnt=INT_MAX;
-
-
-    for(int i =1;i*i<=n;i++){
-        int res =1+ solve(n-i*i);
-        cnt= min(cnt,res);
-    }
-    return dp[n]=cnt;
-}
     int numSquares(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=0;
+        int cnt = 1;
+        while(cnt*cnt <= n) {
+        int sq = cnt*cnt;
+        for(int i = sq; i < n+1; i++) {
+            dp[i] = min(dp[i-sq] + 1,dp[i]);
+        }
+        cnt++;
+    }
+    return dp[n];
     }
 };
