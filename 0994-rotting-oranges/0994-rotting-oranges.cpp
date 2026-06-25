bafb1f8bb646ab5602ac780f4835a513130fd4ed
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-25 12:15:05 +0530

    Time: 0 ms (100%), Space: 16.8 MB (52.6%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-
-// void bfs(int node,int n,int m,vector<vector<int>>&grid,vector<int>&vis){
-
-// }
-
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(),m=grid[0].size();
-       // vector<int>vis(n,0);
+        int n =grid.size(),m=grid[0].size();
+
         queue<pair<int,int>>q;
-        int time=0;
+        int time =0;
         int fresh=0;
-        for(int i =0;i<n;i++){
+        for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(grid[i][j]==2) q.push({i,j});
                 if(grid[i][j]==1) fresh++;
-                            }
+            }
         }
         if(fresh==0) return 0;
+        
+        vector<pair<int,int>>dir= {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty()&& fresh>0){
+            int sz =q.size();
+            if(fresh==0) return 0;
+             time++;
 
-vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!q.empty() &&fresh>0){
-            int sz=q.size();
-            for(int  i=0;i<sz;i++){
-                int row =q.front().first;
-                int col =q.front().second;
-                q.pop();
-
-                for(auto [dr,dc]:dir){
-                    int r= row+dr;
-                    int c= col+dc;
-                    if(r>=0&&c>=0&&r<n&&c<m&&grid[r][c]==1){
-                        grid[r][c]=2;
-                        fresh--;
-                        q.push({r,c});
-                    }
-                }
+            for(int i=0;i<sz;i++){
+             auto[r,c] =q.front();q.pop();
+            for(auto[dr,dc]:dir){
+                int nr = r+dr;
+                int nc=c+dc;
+            if(nr>=0&&nc>=0&&nr<n&&nc<m&&grid[nr][nc]==1){
+                grid[nr][nc]=2;
+                fresh--;
+                q.push({nr,nc});
             }
-            time++;
-        }
-        return fresh==0? time:-1;
 
+            }
+            }
+        }
+        return (fresh==0)?time:-1; 
     }
 };
