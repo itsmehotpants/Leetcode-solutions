b73c59c84b17bfb68cf269d3afb1d7a7d2993cef
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-06-30 01:13:53 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,27 +1,13 @@
-#include <cstring>
 class Solution {
 public:
-    int dp[10001];
-
-    bool dfs(vector<int>& nums, int i) {
-        if (i == nums.size() - 1)
-            return dp[i] = 1;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        int end = min((int)nums.size() - 1, i + nums[i]);
-
-        for (int j = i + 1; j <= end; j++) {
-            if (dfs(nums, j))
-                return dp[i] = 1;
-        }
-
-        return dp[i] = 0;
-    }
-
     bool canJump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(nums, 0);
+        int goal = nums.size()-1;
+        for(int i = nums.size()-2;i>=0;i--){
+            if(nums[i]+i>=goal){
+                goal = i;
+            }
+
+        }
+        return goal==0;
     }
 };
