30fe55d5f85166b0d21ee3a2bfdb44e270a56aea
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-06 05:33:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (48.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
 
-bool dfs(int node , vector<vector<int>>&adj,vector<bool>&vis,vector<bool>&path){
-    vis[node]=1,path[node]=1;
+bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis,vector<int>&path){
+vis[node]=1,path[node]=1;
+for(int i:adj[node]){
+    if(!vis[i]){
+        if(dfs(i,adj,vis,path)) return true;
 
-    for(int i:adj[node]){
-        if(!vis[i]){
-            if(dfs(i,adj,vis,path)) return true;;
-        }
-        else if(path[i]==1) return true;
     }
-    path[node]=false;
-    return false;
+    else if(path[i]==1) return true;
 }
-    bool canFinish(int n, vector<vector<int>>& pre) {
+path[node]=false;
+return false;
+
+    }
+
+    bool canFinish(int n, vector<vector<int>>& preq) {
         vector<vector<int>>adj(n);
-        for(const auto &e:pre){
-            int u =e[0],v=e[1];
-            adj[u].push_back(v);
-        }
-        vector<bool>vis(n,0);
-        vector<bool>path(n,0);
+        for(auto &e:preq){
+            adj[e[0]].push_back(e[1]);
+        }   
+        vector<int>vis(n,0); 
+        vector<int>path(n,0);
         for(int i =0;i<n;i++){
             if(!vis[i]){
-                if(dfs(i,adj,vis,path)) return false;
+               if(dfs(i,adj,vis,path)) return false;
             }
-        }
-        return true;
+        } 
+        return true;   
+
     }
 };
