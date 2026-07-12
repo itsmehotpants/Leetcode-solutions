64dc9afc4dff41616406fa034d811b91d23d21c8
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-12 06:18:18 +0530

    Time: 67 ms (23.08%), Space: 43.5 MB (75.05%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> arrayRankTransform(vector<int>& arr) {
+//         vector<int>temp =arr;
+//       //  sort(temp.begin(),temp.end());
+//         map<int,int>mp;
+//         for(int i=0;i<temp.size();i++){
+//             if(!mp.count(temp[i])){
+//                 mp[temp[i]] = mp.size()+1;
+//             }
+//         }
+
+//         vector<int>res;
+//         for(int& i:arr){
+//            i = mp[i];
+//         }
+//         return arr;
+
+//     }
+// };
+
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int>temp =arr;
-        sort(temp.begin(),temp.end());
-        unordered_map<int,int>mp;
-        for(int i=0;i<temp.size();i++){
-            if(!mp.count(temp[i])){
-                mp[temp[i]] = mp.size()+1;
-            }
-        }
+        map<int,int> mp;
 
-        vector<int>res;
-        for(int& i:arr){
-           i = mp[i];
-        }
+        for(int x:arr)
+            mp[x];
+
+        int rank=1;
+
+        for(auto &p:mp)
+            p.second=rank++;
+
+        for(int &x:arr)
+            x=mp[x];
+
         return arr;
-
     }
 };
