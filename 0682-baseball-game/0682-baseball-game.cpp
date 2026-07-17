96c9f6c6ddff5233d67ef636f6b92e1ea3ea84d4
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-17 14:45:54 +0530

    Time: 0 ms (100%), Space: 12.8 MB (34.65%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        int n = operations.size();
+        stack<int> st;
+        int scores = 0;
+        for (string op : operations) {
+            if (op == "D") {
+                st.push(2 * st.top());
+            } else if (op == "C") {
+                st.pop();
+            } else if (op == "+") {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.push(a);
+                st.push(a + b);
+            } else {
+                st.push(stoi(op));
+            }
+        }
+
+        if (st.empty())
+            return 0;
+        while (!st.empty()) {
+            scores += st.top();
+            st.pop();
+        }
+        return scores;
+    }
+};
