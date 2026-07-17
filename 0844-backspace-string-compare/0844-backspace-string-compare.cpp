1de2943c326f0fad4c210395a3392a6a5bd0762a
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-17 12:54:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.74%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1,s2;
+        for(char c:s){
+            if(c!='#') s1.push(c);
+            else{
+                if(s1.empty()) continue;
+                else s1.pop();
+            }
+        }
+         for(char c:t){
+            if(c!='#') s2.push(c);
+            else{
+                if(s2.empty()) continue;
+                else s2.pop();
+            }
+        }
+        return s1==s2;
+
+    }
+};
