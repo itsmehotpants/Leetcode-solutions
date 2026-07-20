ee5005224a12b23236756268ea1af01fbaea1ea6
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-20 12:28:03 +0530

    Time: 0 ms (100%), Space: 55.1 MB (31.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,11 +1,22 @@
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         unordered_map<int,int>mp;
+//         for(int i:nums) mp[i]++;
+//         for(int i =1;i<INT_MAX;i++){
+//             if(!(mp.count(i)>0)) return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i:nums) mp[i]++;
-        for(int i =1;i<INT_MAX;i++){
-            if(!(mp.count(i)>0)) return i;
-        }
-        return -1;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
+        for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
+        for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
+        return n + 1;
     }
 };
