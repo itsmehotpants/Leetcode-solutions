a75ad4ab6f7fce0d5af5319ced97e73001d71e68
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-22 08:42:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
+int n,m;
+vector<pair<int,int>>dir= {{1,0},{0,1},{-1,0},{0,-1}};
+
+void dfs(vector<vector<int>>& a, int i, int j, int col,int val){
+    if(i<0||j<0||i>=n||j>=m||a[i][j]==col||a[i][j]!=val) return;
+    a[i][j]=col;
+    for(auto &[u,v]:dir){
+        dfs(a,i+u,j+v,col,val);
+    }
+
+}
+
     vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int col) {
-        int n =a.size(),m=a[0].size();
-        int og = a[sr][sc];
-        if(og==col) return a;
-
-
-        vector<pair<int,int>>dis = {{1,0},{0,1},{-1,0},{0,-1}};
-        queue<pair<int,int>>q;
-        //vector<vector<int>>adj;
-        q.push({sr,sc});
-        while(!q.empty()){
-            auto [x,y] =q.front();q.pop();
-            a[x][y]=col;
-            for(auto&dir:dis){
-                int nx =x+dir.first;
-                int ny =y+dir.second;
-
-                if(nx>=0&&nx<n&&ny>=0&&ny<m&&a[nx][ny]==og){
-                    a[nx][ny]=col;
-                    q.push({nx,ny});
-                } 
-            }
-        } 
+        n = a.size();m=a[0].size();
+        int val =a[sr][sc];
+        dfs(a,sr,sc,col,val);
         return a;
     }
 };
