0e583fd76c6637b806a921b34d163f8911f48ddf
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-22 08:35:13 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,28 @@
 class Solution {
-        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-    void dfs(vector<vector<int>>& img, int color, int i, int j, int val){
-        if(i<0 || j<0 || i>=img.size() || j>=img[i].size() || img[i][j]!=val || img[i][j] == color ) return;
-        img[i][j]=color;
-        // dfs(img, color, i+1, j, val);
-        // dfs(img, color, i-1, j, val);
-        // dfs(img, color, i, j-1, val);
-        // dfs(img, color, i, j+1, val);
-        for(auto [u,v]:dir){
-            dfs(img,color,i+u,j+v,val);
-        }
-    
-    }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
-        int val = img[sr][sc];
-        dfs(img, color, sr, sc, val);
-        return img;
+    vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int col) {
+        int n =a.size(),m=a[0].size();
+        int og = a[sr][sc];
+        if(og==col) return a;
+
+
+        vector<pair<int,int>>dis = {{1,0},{0,1},{-1,0},{0,-1}};
+        queue<pair<int,int>>q;
+        //vector<vector<int>>adj;
+        q.push({sr,sc});
+        while(!q.empty()){
+            auto [x,y] =q.front();q.pop();
+            a[x][y]=col;
+            for(auto&dir:dis){
+                int nx =x+dir.first;
+                int ny =y+dir.second;
+
+                if(nx>=0&&nx<n&&ny>=0&&ny<m&&a[nx][ny]==og){
+                    a[nx][ny]=col;
+                    q.push({nx,ny});
+                } 
+            }
+        } 
+        return a;
     }
 };
