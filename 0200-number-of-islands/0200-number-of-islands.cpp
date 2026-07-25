e9294fe44d189461157572d866c3e50cfea48c5f
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-07-25 11:17:28 +0530

    Time: 19 ms (96.46%), Space: 23.3 MB (29.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,35 @@
 class Solution {
 public:
-vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
-void bfs(int n,int m,vector<vector<char>>& grid){
 
-    queue<pair<int,int>> q;
-
-    q.push({n,m});
-    grid[n][m]='0';
-
-    while(!q.empty()){
-
-        auto [i,j]=q.front();
-        q.pop();
-
-        for(auto [dx,dy]:dir){
-
-            int x=i+dx;
-            int y=j+dy;
-
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size())
-                continue;
-
-            if(grid[x][y]=='1'){
-
-                grid[x][y]='0';
-
-                q.push({x,y});
-            }
+vector<pair<int,int>>dir = {{1,0},{0,1},{-1,0},{0,-1}};
+int m,n;
+void bfs(int i,int j ,vector<vector<char>>& grid){
+queue<pair<int,int>>q;
+q.push({i,j});
+grid[i][j]='0';
+while(!q.empty()){
+    auto [x,y] =q.front();q.pop();
+    for(int k =0;k<4;k++){
+        int nx = x +dir[k].first;
+        int ny= y+dir[k].second;
+        if(nx>=0&&ny>=0&&nx<m&&ny<n&&grid[nx][ny]=='1'){
+            grid[nx][ny]='0';
+            q.push({nx,ny});
         }
     }
 }
-
+}
     int numIslands(vector<vector<char>>& grid) {
-        int n =grid.size(),m=grid[0].size();
-        int ans=0;
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
+         m = grid.size(),n=grid[0].size();
+int ans=0;
+        for(int i = 0;i<m;i++){
+            for(int j =0;j<n;j++){
                 if(grid[i][j]=='1'){
                     ans++;
                     bfs(i,j,grid);
                 }
             }
-            
         }
         return ans;
-
     }
 };
