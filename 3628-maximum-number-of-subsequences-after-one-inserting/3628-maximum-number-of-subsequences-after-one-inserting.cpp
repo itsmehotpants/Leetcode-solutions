da6fd43ab05049a81fc271467415e536ac906865
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-09 15:39:05 +0530

    Time: 60 ms (14.63%), Space: 72.5 MB (13.43%) - LeetHub

--- 3628-maximum-number-of-subsequences-after-one-inserting/3628-maximum-number-of-subsequences-after-one-inserting.cpp
+++ 3628-maximum-number-of-subsequences-after-one-inserting/3628-maximum-number-of-subsequences-after-one-inserting.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     long long numOfSubsequences(string s) {
-        int n = s.size();
-
-        long long countL = 0, countLC = 0, countLCT = 0;
-        for (char ch : s) {
-            if (ch == 'L') countL++;
-            else if (ch == 'C') countLC += countL;
-            else if (ch == 'T') countLCT += countLC;
+        long long  n=s.size();
+        long long  cntL=0,cntLC=0,cntLCT=0;
+        for(char c:s){
+            if(c=='L') cntL++;
+            else if(c=='C') cntLC+=cntL;
+            else if(c=='T') cntLCT+=cntLC;
+        }
+        vector<long long >prefL(n+1,0),prefLC(n+1,0),sufT(n+1,0),sufCT(n+1,0);
+        for(long long  i=0;i<n;i++){
+            prefL[i+1]=prefL[i]+(s[i]=='L'?1:0);
+            prefLC[i+1] = prefLC[i]+(s[i]=='C'?prefL[i]:0);
         }
 
-        vector<long long> prefixL(n + 1, 0), prefixLC(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            prefixL[i+1] = prefixL[i] + (s[i] == 'L' ? 1 : 0);
-            prefixLC[i+1] = prefixLC[i] + (s[i] == 'C' ? prefixL[i] : 0);
-        }
-        vector<long long> suffixT(n + 1, 0), suffixCT(n + 1, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            suffixT[i] = suffixT[i+1] + (s[i] == 'T' ? 1 : 0);
-            suffixCT[i] = suffixCT[i+1] + (s[i] == 'C' ? suffixT[i+1] : 0);
+         for(long long  i=n-1;i>=0;i--){
+            sufT[i]=sufT[i+1]+(s[i]=='T'?1:0);
+            sufCT[i] = sufCT[i+1]+(s[i]=='C'?sufT[i+1]:0);
         }
 
-        long long maxGain = 0;
-        for (int i = 0; i <= n; i++) {
-            long long gainL = suffixCT[i];
-            long long gainC = prefixL[i] * suffixT[i];
-            long long gainT = prefixLC[i];
-            maxGain = max({maxGain, gainL, gainC, gainT});
-        }
+        long long  maxans=0;
+        for(long long  i =0;i<=n;i++){
+            long long  gainL = sufCT[i];
+            long long  gainC = prefL[i]*sufT[i];
+            long long  gainT  = prefLC[i];
+            maxans = max({maxans,gainL,gainC,gainT });
 
-        return countLCT + maxGain;
+        }
+        return maxans+cntLCT;
+        
+
+        
     }
 };
