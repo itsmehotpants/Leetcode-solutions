891325adc6712107474947829cca78e1eafbea42
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-09 11:49:46 +0530

    Time: 74 ms (50.56%), Space: 14.3 MB (61.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,46 +1,42 @@
 // class Solution {
 // public:
-   
-//     int lengthOfLIS(vector<int>& nums) {
-// int n =nums.size();
-// vector<int>dp(n,1);
-// int ans=1;
-//     for(int i =0;i<n;i++){
-//         for(int j=0;j<i;j++){
-//             if(nums[i]>nums[j]){
-//                 dp[i] = max(dp[i],dp[j]+1);
-//                 ans= max(ans,dp[i]);
-//             }}}
-//             return ans;
-//         }
-    
-//     };
-//     // int solve(vector<int>& nums, int i, int prev_i) {
-//     //     if(i >= size(nums)) return 0;
-//     //     if(dp[prev_i+1] != -1) return dp[prev_i+1];
-//     //     int take = 0, dontTake = solve(nums, i + 1, prev_i);
-//     //     if(prev_i == -1 || nums[i] > nums[prev_i])
-//     //         take = 1 + solve(nums, i + 1, i);
-//     //     return dp[prev_i+1] = max(take, dontTake);
-//     // }
-// // };
+// int n;
+// int dp[2501][2501];
+// int solve(vector<int>& nums, int i, int p){
+//     if(i>=n) return 0;
+//     if(p!=-1 && dp[i][p]!=-1) return dp[i][p];
+//     int take =0;
+//     if(p==-1||nums[i]>nums[p]){
+//         take = 1+solve(nums,i+1,i);
+//     }
+//     int skip = solve(nums,i+1,p);
+//     if(p!=-1){
+//     dp[i][p]=max(take,skip);
+//     }
+//     return dp[i][p=max(take,skip);
 
-//patience sorting 
+// }
+//     int lengthOfLIS(vector<int>& nums) {
+//         n=nums.size();
+//         memset(dp,-1,sizeof(dp));
+//         return solve(nums,0,-1);
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> sorted;
-        
-        for(int i = 0; i<n; i++) {
-            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
-            
-            if(it == end(sorted))
-                sorted.push_back(nums[i]); 
-            else
-                *it = nums[i];
+        int maxLis=1;
+        int n =nums.size();
+        vector<int>dp(n,1);
+        for(int  i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                    maxLis = max(maxLis,dp[i]);
+                }
+            }
         }
-        
-        return (int)sorted.size();
+        return maxLis;
     }
 };
