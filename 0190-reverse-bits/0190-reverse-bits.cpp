a74b0b8d76da3f9be02ffeedd72fc543324166fd
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-18 16:17:47 +0530

    Time: 4 ms (28.58%), Space: 8.2 MB (73.24%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int rev = 0;
+        int res = 0;
         for(int i = 0; i < 32; i++) {
-            rev = (rev << 1) | ((n >> i) & 1);
+           res = res<<1;
+           res |= n&1;
+           n>>=1;
         }
-        return rev;
+        return res;
     }
 };
