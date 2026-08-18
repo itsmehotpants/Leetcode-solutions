a0932aaef6cc621a7c9f8b90ee31119bd3d2ed4f
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-18 14:26:03 +0530

    Time: 0 ms (100%), Space: 77 MB (76.22%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* dummy = new ListNode();
-    ListNode* temp = dummy;
-    int carry=0;
-    while(l1!=NULL||l2!=NULL||carry){
-        int sum=0;
-        if(l1!=NULL){
-            sum+=l1->val;
-            l1=l1->next;
-            
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* tail = dummyHead;
+        int carry = 0;
+        while(l1!=nullptr || l2!=nullptr || carry!=0){
+            int dig1 = (l1!=nullptr)? l1->val:0;
+            int dig2 = (l2!=nullptr)? l2->val:0;
+
+            int sum =dig1+dig2+carry;
+            int dig =sum%10;
+            carry = sum/10;
+
+            ListNode* newNode = new ListNode(dig);
+            tail->next =newNode;
+            tail = tail->next;
+
+            l1 = (l1!=nullptr) ? l1->next:nullptr;
+            l2 = (l2!=nullptr) ? l2->next:nullptr;
         }
-        if(l2!=NULL){
-            sum+=l2->val;
-            l2=l2->next;
-            
-        }
-        sum+=carry;
-        carry=sum/10;
-        ListNode*node = new ListNode(sum%10);
-        temp->next=node;
-        temp=temp->next;
-        
-    }
-        return dummy->next;
+        ListNode* res = dummyHead->next;
+        // delete dummyHead;
+        return res;
     }
 };
