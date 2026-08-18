872004f41cd38c7547118736372228a040dea6fb
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-18 13:44:00 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.59%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& a) {
+        int n =a.size();
+        for(int i=n-1;i>=0;i--){
+            if(a[i]<9){
+                a[i]+=1;
+                return a;
+            }
+            a[i]=0;
+        }
+        a.insert(a.begin(),1);
+        return a;
+    }
+};
