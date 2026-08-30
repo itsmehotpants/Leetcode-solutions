df67634c3124368dad22d421b7a3b21334010ece
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-31 03:48:59 +0530

    Time: 0 ms (100%), Space: 30.9 MB (23.43%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -14,13 +14,43 @@ int size=1;
 
     }
 
+   int bfs(int i,int j,vector<vector<int>>& grid){
+    int ans = 0;
+
+    queue<pair<int,int>> q;
+    q.push({i,j});
+    grid[i][j] = 0;
+
+    while(!q.empty()){
+        auto [x,y] = q.front();
+        q.pop();
+
+        ans++;
+
+        for(int k = 0; k < 4; k++){
+            int nx = x + dir[k].first;
+            int ny = y + dir[k].second;
+
+            if(nx >= 0 && ny >= 0 &&
+               nx < m && ny < n &&
+               grid[nx][ny] == 1){
+
+                grid[nx][ny] = 0;
+                q.push({nx,ny});
+            }
+        }
+    }
+
+    return ans;
+}
+
 int maxAreaOfIsland(vector<vector<int>>& grid) {
         m =grid.size(),n=grid[0].size();
         int ans= 0;
         for(int i=0;i<m;i++){
             for(int j =0;j<n;j++){
                 if(grid[i][j]==1){
-                    ans = max(ans,dfs(i,j,grid));
+                    ans = max(ans,bfs(i,j,grid));
                 }
             }
         }
