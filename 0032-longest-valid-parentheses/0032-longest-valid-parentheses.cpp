909b9bcea59674a0a45c0b39039536f41679f32e
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-31 00:46:29 +0530

    Time: 7 ms (8.96%), Space: 11.8 MB (50.27%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,36 +1,59 @@
-class Solution {
-    public:
-    int longestValidParentheses(string s) {
-        int n=s.size();
-        int open =0,close=0;
-        int maxLen =0;
+// class Solution {
+//     public:
+//     int longestValidParentheses(string s) {
+//         int n=s.size();
+//         int open =0,close=0;
+//         int maxLen =0;
 
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                open++;
-            }
-            else close++;
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]=='('){
+//                 open++;
+//             }
+//             else close++;
             
-                if(open==close){
-                    int len =open+close;
-                    maxLen=max(maxLen,len);
-                }
-                else if(close>open){
-                    open =0,close=0;
-                }
+//                 if(open==close){
+//                     int len =open+close;
+//                     maxLen=max(maxLen,len);
+//                 }
+//                 else if(close>open){
+//                     open =0,close=0;
+//                 }
 
-            }
+//             }
 
-        open =close=0;
-        for(int i =n-1;i>=0;i--){
-            if(s[i]=='(') open++;
-            else close++;
-            if(open==close){
-                int len =open +close;
-                maxLen = max(maxLen,len);
+//         open =close=0;
+//         for(int i =n-1;i>=0;i--){
+//             if(s[i]=='(') open++;
+//             else close++;
+//             if(open==close){
+//                 int len =open +close;
+//                 maxLen = max(maxLen,len);
+//             }
+//             else if(open>close) open=close=0;
+//         }
+//         return maxLen;
+//     }
+// };
+
+
+class Solution{
+    public:
+    int longestValidParentheses(string s){
+        int n = s.length();
+        stack<int>idx;
+        idx.push(-1);
+        int mx= 0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                idx.push(i);
             }
-            else if(open>close) open=close=0;
+            else{
+                idx.pop();
+                if(idx.empty()) idx.push(i);
+                else mx = max(mx,i-idx.top());
+            }
         }
-        return maxLen;
+        return mx;
     }
 };
