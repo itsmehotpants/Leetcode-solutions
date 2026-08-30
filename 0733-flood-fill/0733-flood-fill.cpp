1bf110ab4014d79330fe59059f2e2e1bdbad5792
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-31 04:27:36 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,21 +1,18 @@
 class Solution {
-public:
-int n,m;
-vector<pair<int,int>>dir= {{1,0},{0,1},{-1,0},{0,-1}};
+        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-void dfs(vector<vector<int>>& a, int i, int j, int col,int val){
-    if(i<0||j<0||i>=n||j>=m||a[i][j]==col||a[i][j]!=val) return;
-    a[i][j]=col;
-    for(auto &[u,v]:dir){
-        dfs(a,i+u,j+v,col,val);
+    void dfs(vector<vector<int>>& img, int color, int i, int j, int val){
+        if(i<0 || j<0 || i>=img.size() || j>=img[i].size() || img[i][j]!=val || img[i][j] == color ) return;
+        img[i][j]=color;
+        for(auto [u,v]:dir){
+            dfs(img,color,i+u,j+v,val);
+        }
+    
     }
-
-}
-
-    vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int col) {
-        n = a.size();m=a[0].size();
-        int val =a[sr][sc];
-        dfs(a,sr,sc,col,val);
-        return a;
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
+        int val = img[sr][sc];
+        dfs(img, color, sr, sc, val);
+        return img;
     }
 };
