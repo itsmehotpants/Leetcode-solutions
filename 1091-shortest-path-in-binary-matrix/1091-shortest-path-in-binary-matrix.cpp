cfd8be5cab232f6f295eccfeefd4b39c1106d316
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-09-02 23:42:49 +0530

    Time: 15 ms (69.86%), Space: 23.6 MB (84.1%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n =grid.size();
-         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        queue<pair<int,pair<int,int>>>q;//pathlength,row,col
-        vector<vector<int>>vis(n,vector<int>(n,0));
-        q.push({1,{0,0}});
-        vis[0][0]=1;
-        int dr[] = {-1,-1,-1,0,0,1,1,1};
-        int dc[] = {-1,0,1,-1,1,-1,0,1};
-        while(!q.empty()){
-            int r = q.front().second.first;
-            int c = q.front().second.second;
-            int d = q.front().first;
-             q.pop();
-        
-        if(r == n-1 && c == n-1) return d;
-        for(int i =0;i<8;i++){
-            int nr =r+dr[i];
-            int nc= c+dc[i];
+        int n  =grid.size();
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
 
-            if(nr>=0&&nc>=0&&nr<n&&nc<n){
-                if(!vis[nr][nc] && grid[nr][nc]==0){
-                    q.push({d+1,{nr,nc}});
-                    vis[nr][nc]=1;
+        vector<int>dr = {-1,-1,-1,0,0,1,1,1};
+        vector<int>dc  = {-1,1,0,1,-1,-1,1,0};
+        queue<pair<int,int>>q;
+        q.push({0,0});
+
+        grid[0][0]=1;
+        while(!q.empty()){
+            auto[r,c]=q.front();q.pop();
+
+            int dist = grid[r][c];
+
+            if(r==n-1&&c==n-1) return dist;
+
+            for (int d = 0; d < 8; d++) {
+
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < n &&
+                    grid[nr][nc] == 0) {
+
+                    grid[nr][nc] = dist + 1;
+
+                    q.push({nr, nc});
                 }
             }
-        }}
-        return -1;}};
+        }
+        return -1;
+    }
+};
