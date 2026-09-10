cf44b8527c833ccf78e8e6bba1098cfff094084f
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-09-10 06:04:59 +0530

    Time: 491 ms (74.27%), Space: 91.2 MB (61.94%) - LeetHub

--- 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
+++ 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
-// int dist[75][75][4][75] //r,c,dist,turns;
     int minCost(vector<vector<int>>& grid, int k) {
-       int m =grid.size(),n=grid[0].size();
-       if(m==1&&n==1) return grid[0][0];
-
-    vector<vector<vector<vector<int>>>>cost(m,vector<vector<vector<int>>>(n,vector<vector<int>>(5,vector<int>(k+1,INT_MAX))));
-
-    priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>q;
-
-    q.push({grid[0][0],0,0,0,0});
-    cost[0][0][0][0]=grid[0][0];
-
-vector<int> dx = {0,0,-1,1};
-        vector<int> dy = {1,-1,0,0};
-    while(!q.empty()){
-        vector<int>val = q.top();
-        q.pop();
-        int c = val[0];
-            int x = val[1];
-            int y = val[2];
-            int d = val[3];
-            int t = val[4];
-
-        if(cost[x][y][d][t]!=c) continue;
-        for(int j = 0;j<4;j++){
-            int nx=x+dx[j],ny=y+dy[j];
-            int nc = t;
-            if(nx<0||ny<0||nx>=m||ny>=n) continue;
-
-            if(d!=0 && d!=j+1) nc++;
-            if(nc>k) continue;
-
-            int costed = c+grid[nx][ny];
-            if(cost[nx][ny][j+1][nc]<=costed) continue;
-
-            cost[nx][ny][j+1][nc]=costed;
-            q.push({costed,nx,ny,j+1,nc});
-        }    
-    }
-
-        int ans = INT_MAX;
-        for(int i=0;i<5;i++){
-            for(int j=0;j<=k;j++){
-                ans = min(ans,cost[m-1][n-1][i][j]);
+        int m = grid.size(), n = grid[0].size();
+        if (m == 1 && n == 1) return grid[0][0];
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        
+        vector<vector<vector<vector<int>>>> dist(m, vector<vector<vector<int>>>(n, 
+            vector<vector<int>>(5, vector<int>(k + 1, INT_MAX))));
+        
+        priority_queue<tuple<int,int,int,int,int>, 
+            vector<tuple<int,int,int,int,int>>, greater<>> pq;
+        
+        dist[0][0][4][0] = grid[0][0];
+        pq.push({grid[0][0], 0, 0, 4, 0});
+        
+        while (!pq.empty()) {
+            auto [cost, r, c, dir, turns] = pq.top();
+            pq.pop();
+            
+            if (r == m - 1 && c == n - 1) return cost;
+            if (cost > dist[r][c][dir][turns]) continue;
+            
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dx[d], nc = c + dy[d];
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                
+                int nt = turns;
+                if (dir != 4 && d != dir) nt++;
+                if (nt > k) continue;
+                
+                int ncost = cost + grid[nr][nc];
+                if (ncost < dist[nr][nc][d][nt]) {
+                    dist[nr][nc][d][nt] = ncost;
+                    pq.push({ncost, nr, nc, d, nt});
+                }
             }
         }
-        return ans==INT_MAX ? -1 : ans; 
+        return -1;
     }
 };
