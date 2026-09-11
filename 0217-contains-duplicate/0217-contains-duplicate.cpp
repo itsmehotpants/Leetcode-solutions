057329bb9e8992049eb0b3712c2e4b15dffcdb32
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-09-11 09:46:30 +0530

    Time: 139 ms (9.63%), Space: 116.2 MB (5.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i :nums){
-            if(mp[i]>=1) return true;
-           else mp[i]++;
-        }
-        return false;
+        // unordered_map<int,int>mp;
+        // for(int i :nums){
+        //     if(mp[i]>=1) return true;
+        //    else mp[i]++;
+        // }
+        // return false;
+        set<int>s(nums.begin(),nums.end());
+        return s.size()<nums.size();
     }
 };
