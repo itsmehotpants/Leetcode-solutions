3df2be52f9cf31d31acaf8bf8c8f80a5dd836326
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-09-11 05:35:04 +0530

    Time: 8 ms (54.16%), Space: 23 MB (58.73%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,40 +1,67 @@
+// class Solution {
+// public:
+
+//     // int f(int n, vector<int>& a) {
+//     //     int t = 0;
+
+//     //     for (int i = 0; i < a.size(); i++) {
+//     //         t += (a[i] + n - 1) / n;
+//     //     }
+
+//     //     return t;
+//     // }
+//     long long f(int n, vector<int>& a) {
+//     long long t = 0;
+
+//     for (int x : a) {
+//         t += x / n;
+//         if (x % n != 0)
+//             t++;
+//     }
+
+//     return t;
+// }
+
+//     int minEatingSpeed(vector<int>& a, int h) {
+//         int l = 1;
+//         int r = *max_element(a.begin(), a.end());
+
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+
+//             if (f(mid, a) <= h)
+//                 r = mid - 1;
+//             else
+//                 l = mid + 1;
+//         }
+
+//         return l;
+//     }
+// };
+
 class Solution {
 public:
-
-    // int f(int n, vector<int>& a) {
-    //     int t = 0;
-
-    //     for (int i = 0; i < a.size(); i++) {
-    //         t += (a[i] + n - 1) / n;
-    //     }
-
-    //     return t;
-    // }
-    long long f(int n, vector<int>& a) {
-    long long t = 0;
-
-    for (int x : a) {
-        t += x / n;
-        if (x % n != 0)
-            t++;
-    }
-
-    return t;
-}
-
-    int minEatingSpeed(vector<int>& a, int h) {
-        int l = 1;
-        int r = *max_element(a.begin(), a.end());
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = *max_element(piles.begin(), piles.end());
+        int ans = r;
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
+            long long hours = 0;
+            for (int p : piles){
+                  hours += p / mid;
+                if (p % mid != 0)
+            hours++;
+            }
 
-            if (f(mid, a) <= h)
+            if (hours <= h) {
+                ans = mid;
                 r = mid - 1;
-            else
+            } else {
                 l = mid + 1;
+            }
         }
 
-        return l;
+        return ans;
     }
 };
